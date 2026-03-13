ec65838a16ac6198339db29418764d3953bae98f
Filip <filipvitko@gmail.com>
2026-03-13 15:32:51 +0100

    fix: bot.py works inside docker now

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,23 +1,41 @@
 import telegram.ext
 import os
-from dotenv import load_dotenv
+from pathlib import Path
+import requests
+if Path(".env").exists():
+    from dotenv import load_dotenv
+    load_dotenv()
 
-load_dotenv()
 TOKEN = os.getenv("TELEGRAM_HTTP_API")
+ID = os.getenv("CHAT_ID")
+
+if not TOKEN:
+    raise ValueError("TELEGRAM_HTTP_API environment variable is not set!")
+
+if not ID:
+    raise ValueError("CHAT_ID environment variable is not set!")
+
+def send_message(message: str):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    data = {
+        "chat_id": ID,
+        "text": message
+    }
+    response = requests.post(url, json=data)
+    return response.json()
 
 async def start(update: telegram.Update, context: telegram.ext.ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! I'm your bot. How can I help you today?")
 
 async def help(update: telegram.Update, context: telegram.ext.ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        """
-        /start -> Start the bot
-        /help -> Show help
-        """
+        "/start -> Start the bot"
+        "/help -> Show help"
     )
 
 def main():
     application = telegram.ext.Application.builder().token(TOKEN).build()
+    send_message("Bot started!")
     application.add_handler(telegram.ext.CommandHandler("start", start))
     application.add_handler(telegram.ext.CommandHandler("help", help))
     application.run_polling()
@@ -25,4 +43,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
